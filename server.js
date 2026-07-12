102e946829808a776288de4a2cbb7a86bdb554f9
baoduong659 <dbao31883@gmail.com>
2026-07-13 01:54:13 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http); // Khởi tạo Socket.io
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+app.use(express.static(__dirname)); // Cho phép server gửi file index.html đi
+
+// Khi có người truy cập web
+io.on('connection', (socket) => {
+    console.log('Một thành viên đã vào xem web');
+
+    // Lắng nghe lệnh cập nhật điểm từ bạn
+    socket.on('updateScore', (data) => {
+        console.log('Đang cập nhật điểm mới...');
+        // Phát dữ liệu mới đến TẤT CẢ mọi người đang mở web
+        io.emit('dataChanged', data); 
+    });
 });
 
 http.listen(process.env.PORT || 3000);
